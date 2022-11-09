e07e87fd8eee6bab830580a74ded41136d183f19
v-rowjee <vedrowjee2001@gmail.com>
2022-11-09 14:42:21 +0400

    create dir

--- Scripts/site.js
+++ Scripts/site.js
@@ -2,3 +2,29 @@
 if (window.history.replaceState) {
     window.history.replaceState(null, null, window.location.href);
 }
+
+$('#createDirForm').submit((e) => {
+    e.preventDefault();
+    return false;
+})
+
+$('#createDir').click(() => {
+    var dirname = $('#dirname').val()
+
+    if (dirname) {
+        $.ajax({
+            type: "POST",
+            url: "/Upload/CreateDir",
+            data: { dirname },
+            dataType: "json",
+            success: (reponse) => {
+                if (reponse) {
+                    window.location.reload();
+                }
+                else {
+                    $('#modal').modal('hide');
+                }
+            }
+        })
+    }
+})
